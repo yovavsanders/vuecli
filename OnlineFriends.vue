960c88256196789adf1bf7fa8d5c54ea8c9d9794
yovavsanders <45664116+yovavsanders@users.noreply.github.com>
2020-11-23 12:49:47 +0200

    Add files via upload

---
+++ OnlineFriends.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="online-friends">
+        <h2>Online Friends</h2>
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name:'OnlineFriends',
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
