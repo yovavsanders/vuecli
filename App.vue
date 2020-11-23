f0e8631401c5f491d3454dea7a6af8e531e6edd6
yovavsanders <45664116+yovavsanders@users.noreply.github.com>
2020-11-23 15:32:08 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    <Navbar />
+    <Blogs />
+  
+    <!-- <AllFriends :friends="friends" @delete="deleteFriend"/>
+    <OnlineFriends :friends="friends"   /> -->
+  </div>
+</template>
+
+<script>
+
+import Navbar from './Navbar'
+// import AllFriends from './AllFriends'
+// import OnlineFriends from './OnlineFriends'
+import Blogs from './Blogs'
+export default {
+  name: 'app',
+  components: {
+    Navbar,
+    Blogs
+    // OnlineFriends,
+    // AllFriends
+  },
+
+  data() {
+    return {
+      title: 'My first Vue app, woo!',
+      // friends: [
+      //   {name: 'Mario', online: true},
+      //   {name: 'Luigi', online: false},
+      //   {name: 'Toad', online: true},
+      //   {name: 'Bowser', online: false},
+      // ]
+    }
+  },
+  methods:{
+    // deleteFriend(payload)
+    // {
+    //     console.log(payload);
+    //     this.friends = this.friends.filter(friend => {
+    //       return friend.name != payload.name;
+    //     })
+    // }
+  }
+}
+</script>
+
+<style>
+h1{
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
---
+++ Blogs.vue
@@ -0,0 +1,52 @@
+<template>
+
+    <div class="blogs">
+        <h2>{{blogTitle}}</h2>
+            <div v-for="post in posts" :key="post.id">
+                <h3> {{post.title}}</h3>
+                <p> {{post.body | snippet}}</p>
+
+            </div>
+     </div>
+</template>
+
+
+<script>
+import axios from'axios'
+
+export default {
+    
+    name: 'Blogs',
+    data(){
+        return{
+            blogTitle:'Blogs',
+            posts:[
+            ]
+        }
+    },
+    beforeCreate(){
+        //alert("beforeCreate hook");
+    },
+    created(){
+         //alert("created hook");
+         axios.get('https://jsonplaceholder.typicode.com/posts')
+         .then(response => {
+             console.log(response);
+             this.posts = response.data;
+         }).catch(err =>{
+             console.log(err);
+         })
+    },
+    beforeUpdate(){
+         //alert("beforeUpdate");
+
+    },
+    methods: {
+        changeTitle(){
+            this.blogTitle = "Amazing Blog Sites"
+
+        }
+
+    }
+}
+</script>
